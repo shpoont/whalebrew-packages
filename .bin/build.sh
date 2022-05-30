e41b2b04f271840662b4fd67559df20edced906d
Leonid Komarovsky <leonid@komarovsky.info>
2022-05-31 00:32:06 +0300

    Fixed build.sh script to change working directory for each package

--- .bin/build.sh
+++ .bin/build.sh
@@ -5,5 +5,7 @@ PACKAGES_FOLDER="packages"
 for PACKAGE_FOLDER in "${PACKAGES_FOLDER}/"*; do
     PACKAGE_NAME="shpoont/whalebrew-${PACKAGE_FOLDER//$PACKAGES_FOLDER\//}"
     echo "Building ${PACKAGE_NAME}.."
-    docker build --no-cache --pull --tag "${PACKAGE_NAME}" --file "${PACKAGE_FOLDER}/Dockerfile" .
+    cd "${PACKAGE_FOLDER}"
+    docker build --no-cache --pull --tag "${PACKAGE_NAME}" .
+    cd -
 done
